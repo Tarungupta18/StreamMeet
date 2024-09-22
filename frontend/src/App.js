43594f4ded793b12b3876e58fb8610dac3da30d3
tarun gupta <tkg21430@gmail.com>
2024-09-23 01:27:29 +0530

    HOF for Auth Guard

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,7 +4,7 @@ import LandingPage from './pages/landing';
 import Authentication from './pages/authentication';
 import { AuthProvider } from './contexts/AuthContext';
 import VideoMeetComponent from './pages/VideoMeet';
-// import HomeComponent from './pages/home';
+import HomeComponent from './pages/home';
 // import History from './pages/history';
 
 function App() {
@@ -22,8 +22,8 @@ function App() {
 
             <Route path='/auth' element={<Authentication />} />
 
-            {/* <Route path='/home's element={<HomeComponent />} />
-            <Route path='/history' element={<History />} /> */}
+            <Route path='/home's element={<HomeComponent />} />
+            {/* <Route path='/history' element={<History />} /> */}
             <Route path='/:url' element={<VideoMeetComponent />} />
           </Routes>
         </AuthProvider>
