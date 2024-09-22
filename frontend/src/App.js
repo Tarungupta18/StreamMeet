55789220ddf3111455206b14c74c2a3ff68bfb7c
tarun gupta <tkg21430@gmail.com>
2024-09-23 00:09:58 +0530

    Video-Audio control

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,7 +3,7 @@ import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
 import LandingPage from './pages/landing';
 import Authentication from './pages/authentication';
 import { AuthProvider } from './contexts/AuthContext';
-// import VideoMeetComponent from './pages/VideoMeet';
+import VideoMeetComponent from './pages/VideoMeet';
 // import HomeComponent from './pages/home';
 // import History from './pages/history';
 
@@ -23,8 +23,8 @@ function App() {
             <Route path='/auth' element={<Authentication />} />
 
             {/* <Route path='/home's element={<HomeComponent />} />
-            <Route path='/history' element={<History />} />
-            <Route path='/:url' element={<VideoMeetComponent />} /> */}
+            <Route path='/history' element={<History />} /> */}
+            <Route path='/:url' element={<VideoMeetComponent />} />
           </Routes>
         </AuthProvider>
 
