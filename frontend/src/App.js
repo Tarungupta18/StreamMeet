5c6937b26d204255ce9ac7855121fc201adb9773
tarun gupta <tkg21430@gmail.com>
2024-09-22 21:11:03 +0530

    auth-setup

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,7 @@
 import './App.css';
 import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
 import LandingPage from './pages/landing';
-// import Authentication from './pages/authentication';
+import Authentication from './pages/authentication';
 // import { AuthProvider } from './contexts/AuthContext';
 // import VideoMeetComponent from './pages/VideoMeet';
 // import HomeComponent from './pages/home';
@@ -20,9 +20,9 @@ function App() {
 
             <Route path='/' element={<LandingPage />} />
 
-            {/* <Route path='/auth' element={<Authentication />} />
+            <Route path='/auth' element={<Authentication />} />
 
-            <Route path='/home's element={<HomeComponent />} />
+            {/* <Route path='/home's element={<HomeComponent />} />
             <Route path='/history' element={<History />} />
             <Route path='/:url' element={<VideoMeetComponent />} /> */}
           </Routes>
